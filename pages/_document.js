665fa3d25a63b6c5fa6d89e61807ad7eafa08061
davien21 <chidiebereekennia@gmail.com>
2021-06-07 23:21:56 +0100

    add lots of stuff

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
         <body className="body-container relative">
           <Main />
           <NextScript />
